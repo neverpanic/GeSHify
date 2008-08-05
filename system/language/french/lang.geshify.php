ec25d919b3622a24235b7e3f51306dd28c4b933d
Clemens Lang <neverpanic@gmail.com>
2008-08-05 15:20:44 +0000

    French (thanks to Fabien Amann) German (by me)

---
+++ system/language/french/lang.geshify.php
@@ -0,0 +1,66 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for extension settings page
+//----------------------------------------
+
+"cache_dir" =>
+"R&eacute;pertoire cache relatif au fichier ext.geshify.php (terminant par un slash)",
+
+"ldelimiter" =>
+"Balise gauche pour le bloc de code GeSHify",
+
+"rdelimiter" =>
+"Balise droite pour le bloc de code GeSHify",
+
+"tag_name" =>
+"Nom du tag pour le bloc de code GeSHify",
+
+"default_type" =>
+"Biblioth&egrave;que de coloration du code &agrave; utiliser par d&egrave;faut lorsqu'aucune n'est sp&eacute;cifi&eacute;e {type|lang}={php|html|css|...}",
+
+"php" =>
+"PHP",
+
+"html" =>
+"HTML pour GeSHi 1.1",
+
+"html4strict" =>
+"HTML pour GeSHi 1.0",
+
+"css" =>
+"CSS",
+
+"cache_cutoff" =>
+"Dur&eacute;e du cache en secondes",
+
+"1.0-stable" =>
+"1.0.7 (stable)",
+
+"1.1-alpha" =>
+"1.1.2 (alpha) - &agrave; utiliser avec pr&eacute;caution",
+
+"geshi_version" =>
+"Version de GeSHi &agrave; utiliser",
+
+"geshi_encoding" =>
+'Encodage &agrave; utiliser pour GeSHi. Les valeurs valides sont celles de la fonction PHP <a href="http://www.php.net/htmlentities">htmlentities</a>',
+
+"default_line" =>
+"Valeur par d&eacute;faut du style pour les num&eacute;ros de lignes lorsqu'aucun n'est sp&eacute;cifi&eacute; en utilisant line={normal|fancy99|none}. Entrer fancy[chiffre] pour activer les num&eacute;ros de lignes 'fancy' pour que chaque [chiffre]&egrave;me num&eacute;ro de lignes soit mis en &eacute;vidence",
+
+"keyword_links" =>
+"Activer les liens de mots-cl&eacute; vers la documentation du langage",
+
+"yes" =>
+"Oui",
+
+"no" =>
+"Non",
+//----------------------------------------
+// END
+''=>''
+);
+?>
---
+++ system/language/german/lang.geshify.php
@@ -0,0 +1,66 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for extension settings page
+//----------------------------------------
+
+"cache_dir" =>
+"Cache-Verzeichnis relativ zum Speicherort der ext.geshify.php-Datei (mit terminierendem Slash)",
+
+"ldelimiter" =>
+"Linker Begrenzer für GeSHify-Block",
+
+"rdelimiter" =>
+"Rechter Begrenzer für GeSHify-Block",
+
+"tag_name" =>
+"Name des GeSHify-Tags",
+
+"default_type" =>
+"Hervorhebungsbibliothek, die genutzt werden soll, wenn nicht via {type|lang}={php|html|css|...} eine explizit angegeben wurde",
+
+"php" =>
+"PHP",
+
+"html" =>
+"HTML für GeSHi 1.1",
+
+"html4strict" =>
+"HTML für GeSHi 1.0",
+
+"css" =>
+"CSS",
+
+"cache_cutoff" =>
+"maximale Cachedauer in Sekunden",
+
+"1.0-stable" =>
+"1.0.7 (stable)",
+
+"1.1-alpha" =>
+"1.1.2 (alpha) - Benutzung auf eigene Gefahr!",
+
+"geshi_version" =>
+"zu benutzende GeSHi-Version",
+
+"geshi_encoding" =>
+'Zeichenkodierung für GeSHi. Mögliche Werte entsprechen den möglichen Werten der PHP-Funktion <a href="http://www.php.net/htmlentities">htmlentities</a>',
+
+"default_line" =>
+"vorgegebener Zeilennummerierungstyp, der benutzt wird, wenn nicht via line={normal|fancy99|none} explizit ein anderer angegeben wurde. Gibt fancy<i>[Zahl]</i> ein, um jede <i>[Zahl]</i>-te Zeile hervorzuheben.",
+
+"keyword_links" =>
+"Gibt an, ob im Normalfall Schlüsselworte Links zur entsprechenden Dokumentation der Software darstellen",
+
+"yes" =>
+"Ja",
+
+"no" =>
+"Nein",
+//----------------------------------------
+// END
+''=>''
+);
+?>
